85efdaaf1addeb8acb5faafcd9df8536cd8080dc
Ray Heil <ray@rayheil.com>
2023-05-04 12:27:53 -0500

    Ignore env in sphinx

--- conf.py
+++ conf.py
@@ -16,9 +16,7 @@ author = 'Ray Heil'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'env']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
